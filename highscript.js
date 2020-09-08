1c685639b43eda1da994f70438915823cfb87250
James Lamb <jameslamb13@gmail.com>
2020-09-07 23:18:41 -0400

    Finishing up highscores page and cleaning up code.

--- highscript.js
+++ highscript.js
@@ -5,8 +5,29 @@
 // add captured score to name with concat
 // add newScore to array that displays in box
 
-var allScores []
 
-    // this is where local storage comes into play.
-var highScorerName = g()
-var highScorerNumber = g()
+var $highScorer = document.querySelector("#highScorer")
+var $clearButtonSpot= document.querySelector("#clearButton")
+
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+    for (var i = 0; i < allScores.length; i++) {
+        var $newLi = document.createElement("li");
+        $newLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        $highScorer.appendChild($newLi);
+    }
+}
+
+var $clearButton = document.createElement("input");
+$clearButton.setAttribute("type", "reset");
+$clearButton.setAttribute("class", "btn btn-danger");
+$clearButton.setAttribute("value", "Clear");
+
+$clearButtonSpot.appendChild($clearButton);
+
+$clearButton.addEventListener("click",function(){
+    localStorage.clear();
+    window.location.href = "highscore.html";
+});
--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 var count = 5
 var totalCount = 60
+var score = 0
+// Timers
 var $timerEl = document.querySelector("#timer");
 var $quizTimerEl = document.querySelector("#quizTimer");
 var $quizTitleEl = document.querySelector(".quizTitle");
-
+var $timerTitleEl = document.querySelector("#quizTimer")
+// Quiz Display
 var quizDisplay = document.querySelector(".questionContainer")
-
+// Questions and Choices
 var $questionTitleEl = document.querySelector("#questionTitle");
 var $questionOneEl = document.querySelector("#a1");
 var $questionTwoEl = document.querySelector("#a2");
 var $questionThreeEl = document.querySelector("#a3");
 var $questionFourEl = document.querySelector("#a4");
 
-
+// Buttons
 var $startButtonEl = document.querySelector("#startButton")
 var $allBtnsEl = document.querySelectorAll(".answerBtn")
 
-
+// Which question we are on
 var questionProgressIndex = 0;
 
-
+// Array of Questions
 var questions = [
     {
         questionTitle: "What is a way to print things in the console?",
@@ -28,7 +31,7 @@ var questions = [
         a2: "document.write()",
         a3: "alert()",
         a4: "All The Above",
-        correctAnswer: "All The Above",
+        correctAnswer: "D",
 
 
     },
@@ -39,7 +42,7 @@ var questions = [
         a2: "A programming language",
         a3: "A starbucks coffee size",
         a4: "A famous poet",
-        correctAnswer: "A programming language",
+        correctAnswer: "B",
 
 
     },
@@ -50,7 +53,7 @@ var questions = [
         a2: "Square Brackets",
         a3: "Curly Brackets",
         a4: "Plus Signs",
-        correctAnswer: "Quotations",
+        correctAnswer: "A",
 
 
     },
@@ -61,18 +64,18 @@ var questions = [
         a2: "net",
         a3: "let",
         a4: "char",
-        correctAnswer: "let",
+        correctAnswer: "C",
 
 
     },
     {
         questionTitle: "What is the Comparison Operator for equal to?",
 
-        a1: "===",
-        a2: "====",
-        a3: ":",
+        a1: "  ===",
+        a2: "  ====",
+        a3: "  :",
         a4: "All The Above",
-        correctAnswer: "===",
+        correctAnswer: "A",
 
 
     },
@@ -83,7 +86,7 @@ var questions = [
         a2: "-=",
         a3: "!notEqual",
         a4: "A and C",
-        correctAnswer: "A and C",
+        correctAnswer: "D",
 
 
     },
@@ -94,7 +97,7 @@ var questions = [
         a2: "&",
         a3: "&&",
         a4: "@",
-        correctAnswer: "&&",
+        correctAnswer: "C",
 
 
     },
@@ -105,7 +108,7 @@ var questions = [
         a2: "pets[1]",
         a3: "pets[2]",
         a4: "pets[3]",
-        correctAnswer: "pets[1]",
+        correctAnswer: "B",
 
 
     },
@@ -116,7 +119,7 @@ var questions = [
         a2: "A way to choose which block of code to run based on whether a condition is true or false.",
         a3: "A named piece of code that executes only when called.",
         a4: "A way to run a block of code over and over a set number of times",
-        correctAnswer: "A named piece of code that executes only when called.",
+        correctAnswer: "C",
 
 
     },
@@ -127,20 +130,27 @@ var questions = [
         a2: "1995",
         a3: "2000",
         a4: "2003",
-        correctAnswer: "1995",
+        correctAnswer: "B",
 
 
     },
+    {
+        questionTitle:"",
+        a1:"",
+        a2:"",
+        a3:"",
+        a4:"",
+        correctAnswer:""
+    }
 
 
 ];
 
-
-
+// Start Button
 $startButtonEl.addEventListener("click", function () {
     quizCountdown();
 });
-
+// Countdown till quiz start
 function quizCountdown() {
     var countdownTimer = setInterval(function () {
         count--;
@@ -157,73 +167,176 @@ function quizCountdown() {
     }, 1000);
 
 };
-
+// Quiz Timer
 function quizTimer() {
     var timerInterval = setInterval(function () {
         totalCount--;
         $quizTimerEl.textContent = totalCount;
         // console.log("wasssipip")
-        if (totalCount === 0) {
-            $quizTimerEl.textContent = "0";
+        if (questionProgressIndex === questions.length - 1) {
+            clearInterval(timerInterval)
+            $timerTitleEl.textContent = "";
+            quizDisplay.textContent = "";
+            $quizTitleEl.style.display = "none";
+            $quizTimerEl.textContent = "";
             gameOver();
-            clearInterval(timerInterval);
+        }
 
+        if (totalCount === 0) {
+            clearInterval(timerInterval)
+            $quizTimerEl.textContent = "";
+            $timerTitleEl.textContent = "";
+            $quizTitleEl.textContent = "";
+            quizDisplay.textContent = "";
+            gameOver();
 
         }
-        
+
     }, 1000);
 
     quizQuestions();
 };
+// Click!!
+for (var i = 0; i < $allBtnsEl.length; i++) [
+    $allBtnsEl[i].addEventListener("click", function (event) {
+        console.log(event.target.id);
+        checkAnswer();
 
+    })
+]
+
+
+// The Main Quiz
+function quizQuestions() {
+
+    $startButtonEl.style.display = "none"
+    quizDisplay.style.display = "block"
+    quizDisplay.setAttribute("class", "card")
+    var q = questions[questionProgressIndex];
+    questionTitles.innerHTML = "<p>" + q.questionTitle + "</p>";
+    a1.innerHTML = q.a1;
+    a2.innerHTML = q.a2;
+    a3.innerHTML = q.a3;
+    a4.innerHTML = q.a4;
+
+}
+
+// Checking if the answer is correct and moving on to the next question.
+function checkAnswer() {
+    var answer = event.target.id;
+
+    // the answer is the same as the correctAnswer
+    if (answer === questions[questionProgressIndex].correctAnswer) {
+        answerIsCorrect();
+
+    }
+    else {
+        answerIsWrong();
+    }
+    if (questionProgressIndex < questions.length - 1) {
+        questionProgressIndex++;
+        quizQuestions();
+    }
+    else {
+        gameOver();
+        $quizTitleEl.textContent = "";
+        quizDisplay.textContent = "";
+        $quizTimerEl.textContent = "";
+
+    }
+}
+// Correct
+function answerIsCorrect() {
+    score++;
+}
+// Wrong
+function answerIsWrong() {
+    totalCount - 9;
+}
+
+
+
+// Game Over
 function gameOver() {
-    alert("Game Over");
-    window.location.href = "highscore.html"
-    clearInterval(timerInterval)
+
+
+    var totalScore = score + totalCount
+
+    alert("Game Over. Your quiz score is " + totalScore);
+    var $gameOverText = document.createElement("text");
+    $gameOverText.setAttribute("id", "gameOverText");
+    $gameOverText.setAttribute("class", "h4")
+    $gameOverText.textContent = "-"
+
+    quizDisplay.appendChild($gameOverText)
+
+    var $createUserSentence = document.createElement("label");
+    $createUserSentence.setAttribute("id", "createUserInput");
+    $createUserSentence.setAttribute("class", "h4")
+    $createUserSentence.textContent = "Enter your initials: ";
+
+    quizDisplay.appendChild($createUserSentence);
+
+    var $createUserInput = document.createElement("input");
+    $createUserInput.setAttribute("class", "text");
+    $createUserInput.setAttribute("id", "initials");
+    $createUserInput.textContent = "Game Over. Your quiz score is " + totalScore;
+
+
+    quizDisplay.appendChild($createUserInput)
+
+    var $submitButton = document.createElement("input");
+    $submitButton.setAttribute("type", "submit");
+    $submitButton.setAttribute("class", "btn btn-primary");
+    $submitButton.setAttribute("value", "Submit");
+
+    quizDisplay.appendChild($submitButton)
+
+    $submitButton.addEventListener("click", function () {
+        var initials = $createUserInput.value
+
+        if (initials === null) {
+            console.log("Initials not recorded")
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: totalScore
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+
+            }
+            allScores.push(finalScore)
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore)
+            window.location.href = "highscore.html"
+        }
+
+    })
+
+
+
+
 };
 
 
 
 
 
-function quizQuestions() {
-quizDisplay.style.display = "block"
- var q = questions[questionProgressIndex];
-questionTitles.innerHTML = "<p>" + q.questionTitle + "</p>";
-a1.innerHTML = q.a1;
-a2.innerHTML = q.a2;
-a3.innerHTML = q.a3;
-a4.innerHTML = q.a4;
-console.log("we are in quiz qs")
 
-}
-// $allBtnsEl.addEventListener("click", function (){
+    // $allBtnsEl.addEventListener("click", function (){
 //     var answer = 
 //     checkAnswer();
 //     console.log(click.event.target);
 // )};
 
+// document.getElementsByClassName("answerBtn").addEventListener("click", event.target);
 
-
-function checkAnswer() {
-    if (event.target == questions[questionProgressIndex].correctAnswer) { answerIsCorrect();
-
-    }
-    else {answerIsWrong();
-    }
-    if (questionProgressIndex < questions.length) {
-        questionProgressIndex ++;
-        quizQuestions();
-    }
-    else {
-        gameOver();
-        clearInterval(timerInterval);
-    }
-}
-// }
-
-
-// var newQuest = document.createElement("p");
+    // var newQuest = document.createElement("p");
 // newQuest.textContent = (questions)[i];
 // $questionTitleEl.appendChild(newQuest);
 
@@ -268,3 +381,6 @@ function checkAnswer() {
 //             var textnode = document.createTextNode(questions.questionTitle);
 //             node.appendChild(textnode);
 //             document.getElementById("questionTitle").appendChild(node); 
+
+// if (questionProgressIndex = questions.length)
+//            $quizTitleEl.textContent = ""
